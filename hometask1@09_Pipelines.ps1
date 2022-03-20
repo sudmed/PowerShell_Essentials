165808a48288570039327631ac12481dc52b2ed3
sudmed <pashkov.dmitriy@gmail.com>
2022-03-21 00:02:09 +0300

    Create hometask1@09_Pipelines.ps1

---
+++ hometask1@09_Pipelines.ps1
@@ -0,0 +1,13 @@
+# PowerShell Essentials Pipelines Homework
+# 1. Write a script that it makes association with running services and process in Windows. Put result in hash table. Use pipelines as match as possible.
+
+$servicesProcesses = @{}
+Get-CimInstance -ClassName Win32_Service | Foreach-Object {
+$_ | Add-Member -MemberType NoteProperty -Name Processes -Value (Get-Process -Id $_.ProcessId) -Passthru | Select-Object Name, Processes } | foreach-object {
+    $services = @{}
+    $object = $_
+    $_ | Get-Member -MemberType noteproperty | Foreach-object { 
+        $services.($_.name) = $object.($_.name)
+    }
+	$servicesProcesses += @{$services.name = $services.Values.ProcessName}
+}
