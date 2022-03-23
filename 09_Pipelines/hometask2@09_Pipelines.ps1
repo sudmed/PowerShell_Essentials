f424de6b8e88f75f257ae018a8e292fdef1f8a41
sudmed <pashkov.dmitriy@gmail.com>
2022-03-23 23:41:20 +0300

    Update and rename hometask2@09_Pipelines.ps1 to 09_Pipelines/hometask2@09_Pipelines.ps1

--- 09_Pipelines/hometask2@09_Pipelines.ps1
+++ 09_Pipelines/hometask2@09_Pipelines.ps1
@@ -1,6 +1,8 @@
 # PowerShell Essentials Pipelines Homework
 # 2. Write a script to find the occurrences of 10 most common words in a given text. Use any article from Wikipedia as given text. Use pipelines as match as possible.
 
+
+<# Code starts here #>
 $text = "Romeo  and  Juliet  is	 a tragedy written	 by William Shakespeare early in his career about two young Italian star-crossed lovers whose deaths ultimately reconcile their feuding families! It was among Shakespeare's most popular plays   during his lifetime and, along with Hamlet, is one of his most frequently performed plays? Today, the title characters are regarded: as archetypal; young lovers.
 Romeo and Juliet belongs to a tradition of tragic romances stretching back to antiquity. The plot is based on an Italian tale translated into verse as The Tragical History of Romeus and Juliet by Arthur Brooke in 1562 and retold in prose in Palace of Pleasure by William Painter in 1567. Shakespeare borrowed heavily from both but expanded the plot by developing a number of supporting characters, particularly Mercutio and Paris. Believed to have been written between 1591 and 1595, the play was first published in a quarto version in 1597. The text of the first quarto version was of poor quality, however, and later editions corrected the text to conform more closely with Shakespeare's original.
 Shakespeare's use of his poetic dramatic structure (especially effects such as switching between comedy and tragedy to heighten tension, his expansion of minor characters, and his use of sub-plots to embellish the story) has been praised as an early sign of his dramatic skill. The play ascribes different poetic forms to different characters, sometimes changing the form as the character develops. Romeo, for example, grows more adept at the sonnet over the course of the play.
@@ -10,9 +12,12 @@ $words = $text -split "'s" -split '[\s+,\.:;\?\!\t()-]' | Where-Object { $_.leng
 $mostCommon = (($words | Group-Object | Sort-Object Count)[-1..-10])
 Write-Host $mostCommon.Name
 $mostCommon
+<# Code ends here #>
 
-#> & .\hometask2@09_Pipelines.ps1
-<# Output
+
+<# Executing the script
+& .\hometask2@09_Pipelines.ps1
+# Output
 
 the and of in to Romeo his Juliet as Shakespeare
 
