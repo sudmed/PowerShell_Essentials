c221d9ad0d64475bda8c9806df580418b667d14d
sudmed <pashkov.dmitriy@gmail.com>
2022-03-23 23:11:49 +0300

    Update hometask1@05_Properties_and_Methods.ps1

--- 05_Properties_and_Methods/hometask1@05_Properties_and_Methods.ps1
+++ 05_Properties_and_Methods/hometask1@05_Properties_and_Methods.ps1
@@ -1,3 +1,6 @@
+# PowerShell Essentials Object Properties and Methods Homework
+# 1. Write a PowerShell class to implement pow(x, n) that is returns the base to the exponent power (base^exponent).
+
 class BaseExponent {
     [double]Pow([double]$x, [double]$n)
     {
