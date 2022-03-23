6c1653fceba5946d5724a067c110ba00741f9de7
sudmed <pashkov.dmitriy@gmail.com>
2022-03-23 23:47:34 +0300

    Update and rename hometask1@11_Providers.ps1 to 11_Providers/hometask1@11_Providers.ps1

--- 11_Providers/hometask1@11_Providers.ps1
+++ 11_Providers/hometask1@11_Providers.ps1
@@ -1,19 +1,19 @@
 # PowerShell Essentials Providers Homework
 # 1. Write a script to get 10 longest words chart and put them in separate file.
 
-  <# Code starts here #>
+
+<# Code starts here #>
 $text = Get-Content text.txt
 $words = $text.split("'s") -split '[\s+,\.:;\?\!\t()-]' -split '[\[\]\{\}]' -split '\d' -replace "'" | Where-Object { $_.length -gt 0 }
 $longestWords = (($words | sort { $_.length })[-1..-10]) > output.txt
-  <# Code ends here #>
+<# Code ends here #>
 
 
-  <# Executing the script
+<# Executing the script
 & .\hometask1@11_Providers.ps1
 Get-Content .\output.txt
-  Executing the script #>
 
-<# Output
+# Output
 transgression
 interchanging
 disparagement
